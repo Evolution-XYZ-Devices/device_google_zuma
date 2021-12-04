37f6e875817264e4bcfe123544507a4534257f3a
Chirayu Desai <chirayudesai1@gmail.com>
2021-12-05 05:07:40 +0530

    zuma: Add device manifest for radio & keymint
    Change-Id: Ib6c4b28d33fb64cb05c715d86a96a829a498beac

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -6,6 +6,11 @@
 
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
+# Manifests
+DEVICE_MANIFEST_FILE += \
+    device/google/zuma/android.hardware.security.rkp-service.citadel.xml \
+    device/google/zuma/manifest_radio_ds.xml
+
 # Partitions
 AB_OTA_PARTITIONS += \
     vendor \
