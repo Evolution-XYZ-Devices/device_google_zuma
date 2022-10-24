9c81dc8d56bf1a988f8470c8b18cbf2f12102b4f
Adam Shih <adamshih@google.com>
2022-10-24 11:25:00 +0800

    isolate legacy sepolicy to start reviewing while letting others work
    Bug: 254378739
    Test: boot, camera, google map, play youtube
    Change-Id: If8ee7c64c0f7534a7e918f36a0e785b0ee9cd11f

--- zuma-sepolicy.mk
+++ zuma-sepolicy.mk
@@ -1,5 +1,5 @@
 # sepolicy that are shared among devices using whitechapel
-BOARD_SEPOLICY_DIRS += device/google/zuma-sepolicy/whitechapel_pro
+BOARD_SEPOLICY_DIRS += device/google/zuma-sepolicy/vendor
 
 # unresolved SELinux error log with bug tracking
 BOARD_SEPOLICY_DIRS += device/google/zuma-sepolicy/tracking_denials
@@ -18,3 +18,10 @@ BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/powerstats
 
 # Health HAL
 BOARD_SEPOLICY_DIRS += device/google/zuma-sepolicy/health
+
+# To be reviewed and removed.
+BOARD_SEPOLICY_DIRS += device/google/zuma-sepolicy/legacy/whitechapel_pro
+PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/zuma-sepolicy/legacy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/google/zuma-sepolicy/legacy/system_ext/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/zuma-sepolicy/legacy/system_ext/private
+
